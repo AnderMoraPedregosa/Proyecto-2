c9d328ca5f6d2cc6a798e63613bbf7118bda7a74
ander <ander.mora@ikasle.egibide.org>
2023-11-13 13:46:49 +0100

    commit login try

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -9,3 +9,51 @@ registrarseBtn.addEventListener('click', () => {
 iniciarSesionBtn.addEventListener('click', () => {
     contenedor.classList.remove("active");
 });
+
+
+
+async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+// EventListener cuando el DOM esté cargado
+document.addEventListener('DOMContentLoaded', function () {
+    const loginButton = document.getElementById('login');
+
+    loginButton.addEventListener('click', async function () {
+        const emailUsuario = document.getElementById('emailUsuario').value;
+        const password = document.getElementById('passwd').value;
+
+        // Llama a la función hashPassword para obtener el hash de la contraseña
+        const hashedPassword = await hashPassword(password);
+
+        // Envia los datos al servidor
+        const response = await fetch('../servidor/bbdd/login.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                emailUsuario: emailUsuario,
+                hashedPassword: hashedPassword,
+            }),
+        });
+        
+        try {
+            const data = await response.json();
+            if (data.success) {
+                console.log('Inicio de sesión exitoso');
+            } else {
+                console.log('Inicio de sesión fallido:', data.error || 'Credenciales incorrectas');
+            }
+        } catch (error) {
+            console.error('Error al analizar la respuesta JSON:', error);
+        }
+        
+    });
+});
---
+++ web/servidor/bbdd/login.php
@@ -0,0 +1,47 @@
+<?php
+require "enumDatosConexion.php"; 
+header('Content-Type: application/json');
+
+// Recibe datos del formulario
+$data = $_POST['emailUsuario'];
+echo $data;
+
+$emailUsuario = $data->emailUsuario;
+$passwordProvided = $data->hashedPassword;
+
+
+$pdo = connect(Datos::Host, Datos::Dbname, Datos::User, Datos::Pass);
+
+if ($pdo) {
+    // La conexión fue exitosa,
+    // Consulta la base de datos para obtener el hash almacenado
+    $stmt = $pdo->prepare("SELECT id, email, password FROM usuarios WHERE email = ?");
+    $stmt->execute([$emailUsuario]);
+    $user = $stmt->fetch();
+
+    // Verifica la contraseña
+    if ($user && password_verify($passwordProvided, $user['password'])) {
+        // Las credenciales son correctas, 
+        echo json_encode(['success' => true]);
+    } else {
+        // Las credenciales son incorrectas
+        echo json_encode(['success' => false]);
+    }
+} else {
+    // No se pudo conectar a la base de datos
+    echo json_encode(['success' => false, 'error' => 'Error de conexión a la base de datos']);
+}
+
+// Función para conectar a la base de datos
+function connect($host, $dbname, $user, $pass)
+{
+    try {
+        $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $dbh;
+    } catch (PDOException $e) {
+        return null; 
+    }
+}
+?>
