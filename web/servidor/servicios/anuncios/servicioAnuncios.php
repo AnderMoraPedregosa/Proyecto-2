a9c7d8a1e4c91b3741c8c36a92b79654a6cf6192
Javier Jamaica <javiernicolas.jamaica@ikasle.egibide.org>
2023-11-14 10:06:42 +0100

    Merge completado

--- web/servidor/servicios/anuncios/servicioAnuncios.php
+++ web/servidor/servicios/anuncios/servicioAnuncios.php
@@ -1,5 +1,5 @@
 <?php
-require "../../bbdd/anunciosCRUD.php";
+require "servidor/bbdd/anunciosCRUD.php";
 
 // Convertir la respuesta a JSON
 function jsonResponse($data, $statusCode = 200)
@@ -15,8 +15,49 @@ if (isset($_POST['accion'])) {
     // Obtener la acción de la solicitud
     $accion = isset($_GET['accion']) ? $_GET['accion'] : '';
 
-// Obtener todos los anuncios
+    // Manejar diferentes acciones con un switch
+    switch ($accion) {
+        case 'anuncios':
+            $anuncios = getAnuncios($dbh);
 
-$anuncios = getAnuncios($dbh);
-jsonResponse($anuncios);
+            if ($anuncios === false) {
+                $response = ['status' => 'error', 'message' => 'No se pudieron obtener los anuncios'];
+                jsonResponse($response, 500);
+            }
 
+            $response = ['status' => 'success', 'data' => $anuncios];
+            jsonResponse($response);
+            break;
+        case 'detalles':
+            // Lógica para la otra acción
+            $response = ['status' => 'success', 'message' => 'Operación realizada con éxito para otra acción'];
+            jsonResponse($response);
+            break;
+        case 'insertarAnuncio':
+            // Separar el id y el nombre de la categoria
+            list($id, $nombreCategoria) = explode('|', $idCategoria);
+
+            $titulo = $_POST["titulo"];
+            $precio = $_POST["precio"];
+            $desc = $_POST["desc"];
+
+            $data = [
+                'titulo' => $titulo,
+                'precio' => $precio,
+                'descripcion' => $desc,
+                'nombre_categoria' => $nombreCategoria,
+                'id_categoria' => $id
+            ];
+
+            insertarAnuncio($dbh, $data);
+            $response = ['status' => 'success', 'message' => 'Operación realizada con éxito para otra acción'];
+
+            jsonResponse($response);
+
+            break;
+        default:
+            $response = ['status' => 'error', 'message' => 'Acción no válida'];
+            jsonResponse($response, 400);
+            break;
+    }
+}
