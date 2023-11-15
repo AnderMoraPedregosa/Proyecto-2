01c2e71992c91c6f8c5df525b280e0e6cbb788ef
Javier Jamaica <javiernicolas.jamaica@ikasle.egibide.org>
2023-11-15 14:16:28 +0100

    Arreglado

--- web/scripts/anuncios.js
+++ web/scripts/anuncios.js
@@ -28,7 +28,7 @@ window.addEventListener("load", async function () {
              <h2>${anuncioNew.titulo}</h2>
              <span class="date">${tiempoTranscurrido}</span>
              <a href="paginas/article.php?accion=detalle&id=${anuncioNew.id}">Leer más</a>
-             <a href="paginas/article.php?accion=editarAnuncio&id=${anuncioNew.id}">Editar</a>
+              <a href="paginas/article.php?accion=editarAnuncio&id=${anuncioNew.id}">Editar</a>
              <div class="clearfix"></div>
          `;
             articles.appendChild(divArticle);
@@ -41,58 +41,6 @@ window.addEventListener("load", async function () {
         articles.appendChild(divArticle);
     }
 });
-
-function datosAnuncios(data) {
-    let anuncios = [];
-
-    data.forEach(async (anuncioJson) => {
-        let divArticle = document.createElement("div");
-        divArticle.className = "article-item";
-        const anuncioNew = new Anuncio(
-            anuncioJson.id,
-            anuncioJson.titulo,
-            anuncioJson.imagen_anuncio,
-            anuncioJson.categoria,
-            anuncioJson.descripcion,
-            anuncioJson.fecha_creacion,
-            anuncioJson.precio,
-            anuncioJson.id_categorias,
-            anuncioJson.id_comercios,
-            anuncioJson.id_comerciante
-        );
-
-            // Agregar la información del anuncio al nuevo elemento div
-            divArticle.innerHTML = `
-             <div class="image-wrap">
-                 <img src="${anuncioNew.imagen}" alt="Producto" />
-             </div>
-             <h2>${anuncioNew.titulo}</h2>
-             <span class="date">${tiempoTranscurrido}</span>
-             <a href="paginas/article.php?accion=detalle&id=${anuncioNew.id}">Leer más</a>
-                <a href="paginas/article.php?accion=editarAnuncio&id=${anuncioNew.id}">Editar</a>
-             <div class="clearfix"></div>
-         `;
-            articles.appendChild(divArticle);
-
-            // Agregar el nuevo elemento div al contenedor de artículos
-        });
-
-
-
-        // Agregar un solo escuchador de eventos al contenedor principal
-        articles.addEventListener("click", function (event) {
-            // Verificar si el clic se realizó en un enlace de editar
-            if (event.target.classList.contains("editarAnuncio")) {
-                // Acciones que deseas realizar cuando se hace clic en editar
-                // Por ejemplo, redirigir a la página de edición
-                event.preventDefault();
-                window.location.href = event.target.href;
-
-            }
-        });
-    } 
-
-
 
 function datosAnuncios(data) {
     let anuncios = [];
