69f0fb38f1b64aed5e00f791aeef714f22fcb34f
beatriz <beatriz.moran@ikasle.egibide.org>
2023-11-18 20:22:12 +0100

    cambios estilos article.php

--- web/paginas/article.php
+++ web/paginas/article.php
@@ -1,33 +1,35 @@
-<?php require "partials/head.php" ?>
+<?php require "partials/head.php"; ?>
 
 <script type="module" src="../../scripts/categoria.js"></script>
 
-<div class="center">
-    <div class="articulos">
-        <div id="content">
-            <script type="module" src="../../scripts/anuncioDetalle.js"></script>
+<div id="prueba">
+    <div class="center">
+        <div class="articulos">
+            <div id="content">
+                <script type="module" src="../../scripts/anuncioDetalle.js"></script>
+            </div>
         </div>
 
-    <div id="editarAnuncio">
-        <?php require "partials/crearEditarAnuncio.php" ?>
+        <div id="editarAnuncio">
+            <?php require "partials/crearEditarAnuncio.php" ?>
+        </div>
+        
+        <aside id="sidebar">
+            <div id="nav-blog" class="sidebar-item">
+                <h3>Puedes hacer esto</h3>
+                <a href="#" class="btn btn-success">Crear artículo</a>
+            </div>
+
+            <div id="search" class="sidebar-item">
+                <h3>Buscador</h3>
+                <p>Encuentra el artículo que buscas</p>
+                <form>
+                    <input type="text" name="search" />
+                    <input type="submit" name="submit" value="Buscar" class="btn" />
+                </form>
+            </div>
+        </aside>
     </div>
-    <aside id="sidebar">
-        <div id="nav-blog" class="sidebar-item">
-            <h3>Puedes hacer esto</h3>
-            <a href="#" class="btn btn-success">Crear artículo</a>
-        </div>
-
-        <div id="search" class="sidebar-item">
-            <h3>Buscador</h3>
-            <p>Encuentra el artículo que buscas</p>
-            <form>
-                <input type="text" name="search" />
-                <input type="submit" name="submit" value="Buscar" class="btn" />
-            </form>
-        </div>
-    </aside>
-
-
-
-    <?php require "partials/footer.php" ?>
 </div>
+
+<?php require "partials/footer.php" ?>
